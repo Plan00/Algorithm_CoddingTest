047df599e960a22526b43a48e2a95c61b534c6d3
Plan00 <xckk12@naver.com>
2024-05-22 13:10:00 +0900

    Create 2071. 평균값 구하기.java

---
+++ SWEA/2071. 평균값 구하기.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+        int sum=0;
+ 
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            sum=0;
+            for(int i = 0; i < 10; i++) {
+                sum += sc.nextInt();
+            }
+            System.out.println("#"+test_case+" "+Math.round(sum/10.0));
+        }
+    }
+}
