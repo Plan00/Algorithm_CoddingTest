03ceda21f14b27189c691a444640a785aec685a5
Plan00 <xckk12@naver.com>
2024-05-24 10:35:30 +0900

    Create 2047. 신문 헤드라인.java

---
+++ SWEA/2047. 신문 헤드라인.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		String T;
+		T=sc.nextLine();
+
+		System.out.println(T.toUpperCase());
+	}
+}
