f8bb117c7862dfd1947c55aa3a65c4be9521d185
Plan00 <xckk12@naver.com>
2024-05-24 10:43:45 +0900

    Create 1545. 거꾸로 출력해 보아요.java

---
+++ SWEA/1545. 거꾸로 출력해 보아요.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+
+		while(T>=0) {
+                    System.out.printf(T+" ");
+                    T--;
+		}
+	}
+}
